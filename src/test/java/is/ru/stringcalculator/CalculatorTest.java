b80d26d2b2d6ca3dcaba8149dd45980a07a4873a
Kevin <kevin13@ru.is>
2014-10-17 18:23:11 +0000

    Changed the test case for testMultipleNumbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -26,12 +26,17 @@ public class CalculatorTest {
 
 	@Test
     public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
+    	assertEquals(10, Calculator.add("1,2,3,4"));
     }
 
     @Test
     public void testNewLine(){
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
+//
+  //  @Test
+ //   public void testNewLine(){
+ //   	assertEquals(3, Calculator.add("//;\n1;2"));
+ //   }
 
 }
